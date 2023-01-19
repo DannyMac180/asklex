1a6acea744c11b4a5eab02c72e7769c81f9024dd
DannyMac180 <mcateerd2@gmail.com>
2023-01-18 22:40:44 -0500

    Get all metdata. Download all audio

--- scripts/add_metadata_to_huggingface_dataset.py
+++ scripts/add_metadata_to_huggingface_dataset.py
@@ -1,44 +1,38 @@
-from googleapiclient.discovery import build
 import os
 import pandas as pd
+import feedparser
+import datasets
 from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_youtube_metadata(channel_id=os.environ.get('LEX_FRIDMAN_POD_CHANNEL_ID')):
-    api_key = os.environ.get('YOUTUBE_API_KEY')
+def get_metadata_from_rss(rss_url):
+    """Get metadata from RSS feed"""
+    feed = feedparser.parse(rss_url)
+    metadata = []
+    for entry in feed.entries:
+        metadata.append({
+            # Populate fields with guid, title, description, enclosure_url, pub_date
+            "guid": entry.guid,
+            "title": entry.title,
+            "description": entry.description,
+            "audio_url": entry.enclosures[0].url,
+            "pub_date": entry.published
+        })
+    return metadata
 
-    # Create service object
-    youtube = build('youtube', 'v3', developerKey=api_key)
-
-    next_page_token = None
-    # Get all the pages of channel metadata from the YouTube v3 API at the specifid channel_id
-    while next_page_token is not "":
-
-        # Retrieve channel metadata
-        response = youtube.search().list(
-            channelId=os.environ.get('LEX_FRIDMAN_POD_CHANNEL_ID'),
-            type='video',
-            part='snippet',
-            maxResults=50,
-            order='date',
-            pageToken=next_page_token
-        ).execute()
-
-        next_page_token = response.get('nextPageToken')
-
-    # Create dataframe containing metadata with video_id as index and title, description, and publishedAt as columns
-    df = pd.DataFrame(columns=["video_id", "title", "publishedAt"])
-    for item in response['items']:
-        df = df.concat({
-            "video_id": item['id']['videoId'],
-            "title": item['snippet']['title'],
-            "publishedAt": item['snippet']['publishedAt']
-        }, ignore_index=True)
-    print(df.head(800))
-
-    # Save dataframe as csv
-    df.to_csv("./dataset/lex_fridman_pod_youtube_metadata.csv", index=False)
+def save_metadata_as_csv_to_hf(metadata, dataset_name="lexfridman_rss_metadata"):
+    """Save metadata as CSV"""
+    csv = pd.DataFrame(metadata).to_csv()
+    # Save CSV to HuggingFace Datasets
+    datasets.save_dataset(data=csv, dataset_name=dataset_name, split="train", overwrite=True)
 
 
-get_youtube_metadata()
+def main():
+    # Get metadata from RSS feed
+    metadata = get_metadata_from_rss("https://lexfridman.com/feed/podcast/")
+    # Save metadata as CSV
+    save_metadata_as_csv_to_hf(metadata)
+
+if __name__ == "__main__":
+    main()
--- scripts/download_audio.py
+++ scripts/download_audio.py
@@ -1,40 +1,20 @@
-from datasets import load_dataset
-from pytube import YouTube
-from pytube.exceptions import RegexMatchError
-from tqdm.auto import tqdm
+import pandas as pd
+import requests
 
-dataset = load_dataset("DannyMac180/lex_fridman_pod_youtube_metadata", split="train")
-print(dataset)
-save_path = "./dataset/audio_files"
+def download_audio(metadata_csv):
+    """Download audio from metadata CSV"""
+    # Load metadata CSV
+    metadata = pd.read_csv(metadata_csv)
+    # Download audio
+    for index, row in metadata.iterrows():
+        # Download audio using requests
+        response = requests.get(row["audio_url"])
+        print(f"Downloading {row['guid']}.mp3, {row['title']}")
+        # Format the 'guid' without https://lexfridman.com/podcast/
+        guid = row["guid"].replace("https://lexfridman.com/?p=", "")
+        # Save mp3 file to audio directory
+        with open(f"/home/dmac/test_dev/asklex.ai/dataset/audio/{guid}.mp3", "wb") as f:
+            f.write(response.content)
 
-def get_audio_files():
-    # Use Pytube to download audio file
-    # https://python-pytube.readthedocs.io/en/latest/user/quickstart.html
-    for row in tqdm(dataset):
-        # Instantiate download url from video_id
-        url = f"https://www.youtube.com/watch?v={row['video_id']}"
-
-        # try to create a YouTube video object
-        try:
-            yt = YouTube(url)
-        except RegexMatchError:
-            print(f"RegexMatchError: {url}")
-            continue
-
-        itag = None
-        # Download only the audio files
-        files = yt.streams.filter(only_audio=True)
-        for file in files:
-            if file.mime_type == "audio/mp4":
-                itag = file.itag
-                break
-        if itag is None:
-            print("NO MP3 AUDIO FOUND")
-            continue
-            
-        # Get the correct mp3 'stream'
-        stream = yt.streams.get_by_itag(itag)
-        # Download the audio file
-        stream.download(output_path=save_path, filename=f"{row['video_id']}.mp3")
-
-get_audio_files()
+if __name__ == "__main__":
+    download_audio("~/test_dev/asklex.ai/dataset/lexfridman_rss_metadata.csv")
