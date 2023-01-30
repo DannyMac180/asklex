50dfa27c43c5010c1794338d1de1358c4ad66596
DannyMac180 <mcateerd2@gmail.com>
2023-01-29 23:12:00 -0500

    Write BigQuery logic for create_embeddings

---
+++ scripts/create_embeddings.py
@@ -0,0 +1,21 @@
+import os
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+
+# Load the service account key JSON file.
+credentials = Credentials.from_service_account_file("./asklex-f6e79ca5e230.json")
+
+# Use the credentials to build a BigQuery client.
+bigquery_client = build("bigquery", "v2", credentials=credentials)
+
+# Make an API request.
+query_results = bigquery_client.jobs().query(
+    projectId="asklex",
+    body={
+        'query': 'SELECT * FROM `asklex.lexfridman_pod_transcriptions` LIMIT 100'
+    }
+).execute()
+
+# Print the results.
+for row in query_results['rows']:
+    print(row['f'])
