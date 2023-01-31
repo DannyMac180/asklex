444c4f6bc97a6249fbdb5b08d50eb843eb0fa883
DannyMac180 <mcateerd2@gmail.com>
2023-01-30 23:14:17 -0500

    Query bq dataset

--- scripts/create_embeddings.py
+++ scripts/create_embeddings.py
@@ -8,11 +8,19 @@ credentials = Credentials.from_service_account_file("./asklex-f6e79ca5e230.json"
 # Use the credentials to build a BigQuery client.
 bigquery_client = build("bigquery", "v2", credentials=credentials)
 
+# # Get the dataset with id 'asklex' from the BigQuery client.
+# dataset = bigquery_client.datasets().get(
+#     projectId="asklex",
+#     datasetId="asklex"
+# ).execute()
+
+# print(dataset)
+
 # Make an API request.
 query_results = bigquery_client.jobs().query(
     projectId="asklex",
     body={
-        'query': 'SELECT * FROM `asklex.lexfridman_pod_transcriptions` LIMIT 100'
+        'query': 'SELECT * FROM asklex.lexfridman_pod_transcriptions LIMIT 100'
     }
 ).execute()
 
