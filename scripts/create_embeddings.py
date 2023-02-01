89fa5088e88231423a03d2f5ac16fba1f429f2d7
DannyMac180 <mcateerd2@gmail.com>
2023-02-01 14:50:41 -0500

    Get segments from BQ in DataFrame

--- scripts/create_embeddings.py
+++ scripts/create_embeddings.py
@@ -1,39 +1,23 @@
 import os
 from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
+from google.cloud import bigquery
 import pandas as pd
 
 # Load the service account key JSON file.
 credentials = Credentials.from_service_account_file("./asklex-f6e79ca5e230.json")
 
 # Use the credentials to build a BigQuery client.
-bigquery_client = build("bigquery", "v2", credentials=credentials)
+bigquery_client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
+sql = """
+    SELECT * FROM asklex.lexfridman_pod_transcriptions LIMIT 10
+"""
 
 # Make an API request.
 try:
-    query_results = bigquery_client.jobs().query(
-        projectId="asklex",
-        body={
-            'query': 'SELECT * FROM asklex.lexfridman_pod_transcriptions LIMIT 10'
-        }
-    ).execute()
+    query_results_df = bigquery_client.query(sql).to_dataframe()
 except Exception as e:
     print(e)
 
-query_results = bigquery_client.jobs().query(
-    projectId="asklex",
-    body={
-        'query': 'SELECT * FROM asklex.lexfridman_pod_transcriptions LIMIT 10'
-    }
-).execute()
-
-table_fields = query_results["schema"]["fields"]
-
-# Get the rows of data in dictionary form from query_results.
-rows = query_results["rows"]
-
-# Convert the query_results to a pandas dataframe, and set the column names from the table_fields 'name element.
-df = pd.DataFrame(rows['f'], columns=[field["name"] for field in table_fields])
-
-# Print the first row of the df.
-print(df.iloc[0])
+# Print the results.
+print(query_results_df.iloc[0])
