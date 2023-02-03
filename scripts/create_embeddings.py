d615391e3bce71dfc64b6036b1975fab3126d1cc
DannyMac180 <mcateerd2@gmail.com>
2023-02-02 23:15:22 -0500

    Create Embeddings

--- scripts/create_embeddings.py
+++ scripts/create_embeddings.py
@@ -1,23 +1,44 @@
 import os
+import openai
 from google.oauth2.service_account import Credentials
 from google.cloud import bigquery
 import pandas as pd
+from dotenv import load_dotenv
 
-# Load the service account key JSON file.
-credentials = Credentials.from_service_account_file("./asklex-f6e79ca5e230.json")
+load_dotenv()
 
-# Use the credentials to build a BigQuery client.
-bigquery_client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+def get_segments_from_bq():
+    # Load the service account key JSON file.
+    credentials = Credentials.from_service_account_file("./asklex-f6e79ca5e230.json")
 
-sql = """
-    SELECT * FROM asklex.lexfridman_pod_transcriptions LIMIT 10
-"""
+    # Use the credentials to build a BigQuery client.
+    bigquery_client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
-# Make an API request.
-try:
-    query_results_df = bigquery_client.query(sql).to_dataframe()
-except Exception as e:
-    print(e)
+    sql = """
+        SELECT * FROM asklex.lexfridman_pod_transcriptions LIMIT 10
+    """
 
-# Print the results.
-print(query_results_df.iloc[0])
+    # Make an API request.
+    try:
+        query_results_df = bigquery_client.query(sql).to_dataframe()
+    except Exception as e:
+        print(e)
+    
+    return query_results_df
+
+def create_embeddings(segments):
+    # Use openai module to create embeddings
+    openai.organization = os.getenv("OPENAI_ORG_ID")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    
+    MODEL = "text-embedding-ada-002"
+
+    # Create embeddings
+    embeddings = openai.Embedding.create(
+        input=segments["text"].tolist(),
+        engine=MODEL
+    )
+
+    print(embeddings)
+    
+create_embeddings(get_segments_from_bq())
