2f6b45ddd4d67bd1f3dd8338b10b5df59d1929fc
DannyMac180 <mcateerd2@gmail.com>
2023-03-03 17:48:38 -0500

    need to debug text embeddings. return off base results

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, request, render_template
 import os
 import pinecone
+import openai
 
 app = Flask(__name__)
 pinecone.init(api_key=os.getenv("PINECONE_API_KEY"),
               environment=os.getenv("PINECONE_ENV"))
-index_name = "asklex"
+index = pinecone.Index('asklex')
 
 @app.route('/', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
         query = request.form['query']
-        results = pinecone.search(index_name, query, k=10)
-        return render_template('results.html', results=results)
+        print(query)
+
+        try:
+            embed = openai.Embedding.create(input=query, engine="text-embedding-ada-002")
+            query = embed["data"][0]["embedding"]
+            results = index.query(query, top_k=5, include_values=False, include_metadata=True)
+            print(results)
+            return render_template('results.html', results=results)
+        except Exception as e:
+            print(e)
+            return render_template('search.html')
+
     return render_template('search.html')
 
 if __name__ == '__main__':
