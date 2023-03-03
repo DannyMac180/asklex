5ecf5babd1952717f3572726ff0f4979ef36e2e9
DannyMac180 <mcateerd2@gmail.com>
2023-03-03 16:46:58 -0500

    basica HTML template and Flask route added

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template
+import os
+import pinecone
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+pinecone.init(api_key=os.getenv("PINECONE_API_KEY"),
+              environment=os.getenv("PINECONE_ENV"))
+index_name = "asklex"
+
+@app.route('/', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form['query']
+        results = pinecone.search(index_name, query, k=10)
+        return render_template('results.html', results=results)
+    return render_template('search.html')
 
 if __name__ == '__main__':
     app.run()
